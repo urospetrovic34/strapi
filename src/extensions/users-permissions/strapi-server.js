c8b62f92d1fdf4404418945b9ae949e30937fc44
UROS <nonamen212@gmail.com>
2022-03-20 11:05:34 +0100

    Reintroduced strapi-server

---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,52 @@
+
+module.exports = (plugin) => {
+    const sanitizeOutput = (user) => {
+      const {
+        password,
+        resetPasswordToken,
+        confirmationToken,
+        ...sanitizedUser
+      } = user; // be careful, you need to omit other private attributes yourself
+      return sanitizedUser;
+    };
+  
+    plugin.controllers.user.me = async (ctx) => {
+      if (!ctx.state.user) {
+        return ctx.unauthorized();
+      }
+      const user = await strapi.entityService.findOne(
+        "plugin::users-permissions.user",
+        ctx.state.user.id,
+        { populate: ["role"] }
+      );
+  
+      ctx.body = sanitizeOutput(user);
+    };
+  
+    plugin.controllers.user.find = async (ctx) => {
+      console.log(ctx.query.filters)
+      const users = await strapi.entityService.findMany(
+        "plugin::users-permissions.user",
+        {
+          populate: { role: true },
+        }
+      );
+      ctx.body = users.map((user) => sanitizeOutput(user));
+    };
+  
+    plugin.controllers.user.findOne = async (ctx) => {
+      if (!ctx.state.user) {
+        return ctx.unauthorized();
+      }
+      const user = await strapi.entityService.findOne(
+        "plugin::users-permissions.user",
+        ctx.params[0].split('/')[2],
+        { populate: ["role"] }
+      );
+  
+      ctx.body = sanitizeOutput(user);
+    };
+  
+    return plugin;
+  };
+  
