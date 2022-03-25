a7a67289676f4643aaf45d49f3e4e87e3b6c9c84
UROS <nonamen212@gmail.com>
2022-03-25 10:03:52 +0100

    Enabled username, it's kind of a pain to config everything so we'll just leave it at that

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,56 +1,54 @@
-
 module.exports = (plugin) => {
-    const sanitizeOutput = (user) => {
-      const {
-        password,
-        resetPasswordToken,
-        confirmationToken,
-        ...sanitizedUser
-      } = user; // be careful, you need to omit other private attributes yourself
-      return sanitizedUser;
-    };
-  
-    plugin.controllers.user.me = async (ctx) => {
-      if (!ctx.state.user) {
-        return ctx.unauthorized();
-      }
-      const user = await strapi.entityService.findOne(
-        "plugin::users-permissions.user",
-        ctx.state.user.id,
-        { populate: ["role"] }
-      );
-  
-      ctx.body = sanitizeOutput(user);
-    };
-  
-    plugin.controllers.user.find = async (ctx) => {
-      console.log(ctx.query.filters)
-      const users = await strapi.entityService.findMany(
-        "plugin::users-permissions.user",
-        {
-          filters:ctx.query.filters,
-          populate: ctx.query.populate,
-          sort:ctx.query.sort,
-          start:ctx.query.start,
-          limit:ctx.query.limit,
-        }
-      );
-      ctx.body = users.map((user) => sanitizeOutput(user));
-    };
-  
-    plugin.controllers.user.findOne = async (ctx) => {
-      if (!ctx.state.user) {
-        return ctx.unauthorized();
-      }
-      const user = await strapi.entityService.findOne(
-        "plugin::users-permissions.user",
-        ctx.params[0].split('/')[2],
-        { populate: ctx.query.populate }
-      );
-  
-      ctx.body = sanitizeOutput(user);
-    };
-  
-    return plugin;
+  const sanitizeOutput = (user) => {
+    const {
+      password,
+      resetPasswordToken,
+      confirmationToken,
+      ...sanitizedUser
+    } = user; // be careful, you need to omit other private attributes yourself
+    return sanitizedUser;
   };
-  
+
+  plugin.controllers.user.me = async (ctx) => {
+    if (!ctx.state.user) {
+      return ctx.unauthorized();
+    }
+    const user = await strapi.entityService.findOne(
+      "plugin::users-permissions.user",
+      ctx.state.user.id,
+      { populate: ["role"] }
+    );
+
+    ctx.body = sanitizeOutput(user);
+  };
+
+  plugin.controllers.user.find = async (ctx) => {
+    console.log(ctx.query.filters);
+    const users = await strapi.entityService.findMany(
+      "plugin::users-permissions.user",
+      {
+        filters: ctx.query.filters,
+        populate: ctx.query.populate,
+        sort: ctx.query.sort,
+        start: ctx.query.start,
+        limit: ctx.query.limit,
+      }
+    );
+    ctx.body = users.map((user) => sanitizeOutput(user));
+  };
+  //comment
+  plugin.controllers.user.findOne = async (ctx) => {
+    if (!ctx.state.user) {
+      return ctx.unauthorized();
+    }
+    const user = await strapi.entityService.findOne(
+      "plugin::users-permissions.user",
+      ctx.params[0].split("/")[2],
+      { populate: ctx.query.populate }
+    );
+
+    ctx.body = sanitizeOutput(user);
+  };
+
+  return plugin;
+};
