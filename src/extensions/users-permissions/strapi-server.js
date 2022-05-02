6cf89b38545d564e672ea422030a8ffd3b4c6ff3
UROS <nonamen212@gmail.com>
2022-05-02 21:30:49 +0200

    Fairfax, VA

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -16,7 +16,7 @@ module.exports = (plugin) => {
     const user = await strapi.entityService.findOne(
       "plugin::users-permissions.user",
       ctx.state.user.id,
-      { populate: ["role"] }
+      { populate: ["role","avatar"] }
     );
 
     ctx.body = sanitizeOutput(user);
