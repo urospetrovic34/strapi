d284eb9d8e23af4d1c27cd17afe9e761d467d419
UROS <nonamen212@gmail.com>
2022-05-10 15:17:55 +0200

    Take that back #6

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -15,7 +15,7 @@ module.exports = createCoreController("api::project.project", ({ strapi }) => ({
         filters: ctx.query.filters,
         populate: ctx.query.populate,
         sort: ctx.query.sort,
-        start: meta.pagination.page * 8,
+        start: meta.pagination.page * 9,
         limit: meta.pagination.pageSize,
       }
     );
